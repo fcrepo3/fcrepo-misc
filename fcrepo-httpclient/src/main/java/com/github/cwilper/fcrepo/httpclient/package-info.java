cdce874501b5681d0d8a9748bab7ab1c8f752321
ajs6f <ajs6f@virginia.edu>
2016-04-15 15:10:20 -0400

    Removing obsolete client tools

--- fcrepo-httpclient/src/main/java/com/github/cwilper/fcrepo/httpclient/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <code>HTTPClient</code> implementations and supporting classes useful for
- * working with Fedora repositories.
- */
-package com.github.cwilper.fcrepo.httpclient;
--- fcrepo-riclient/src/main/java/com/github/cwilper/fcrepo/riclient/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * A client and supporting classes for performing RDF queries against a Fedora
- * repository.
- */
-package com.github.cwilper.fcrepo.riclient;
